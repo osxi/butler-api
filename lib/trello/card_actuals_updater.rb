5bc532ae96c37af3bd8d8d5341d121d9f0ba153a
Jake Craige <jake@poeticsystems.com>
2014-11-05 14:55:00 -0600

    add Trello::CardActualsUpdater#update_cards

--- lib/trello/card_actuals_updater.rb
+++ lib/trello/card_actuals_updater.rb
@@ -1,11 +1,15 @@
 module Trello
   class CardActualsUpdater < Struct.new(:trello_client)
-    def update_card(card_id, update_from_freshbooks:false)
-      run_freshbooks_update if update_from_freshbooks
-
+    def update_card(card_id)
       hours = records_for_card(card_id).sum(:hours)
 
       trello_client.update_card_hours(card_id, hours)
+    end
+
+    def update_cards(*card_ids)
+      # How can I do this?
+      # card_ids.each(&:update_card)
+      card_ids.each { |card_id| update_card(card_id) }
     end
 
     private
--- spec/lib/trello/card_actuals_updater_spec.rb
+++ spec/lib/trello/card_actuals_updater_spec.rb
@@ -14,7 +14,26 @@ describe Trello::CardActualsUpdater do
       subject.trello_client = spy
 
       subject.update_card('28Pjr2dK')
+
       expect(spy).to have_received(:update_card_hours).with('28Pjr2dK', 0.5)
+    end
+  end
+
+  context '#update_cards' do
+    before do
+      TimeEntry.create(trello_card_id: '28Pjr2dK', hours: 0.5)
+      TimeEntry.create(trello_card_id: 'abc12345', hours: 1.5)
+    end
+
+    it 'updates the cards with total hours in db' do
+      spy = spy('subject.trello_client')
+      subject.trello_client = spy
+
+      subject.update_cards('28Pjr2dK', 'abc12345')
+
+      expect(spy).to have_received(:update_card_hours).with('28Pjr2dK', 0.5)
+      expect(spy).to have_received(:update_card_hours).with('abc12345', 1.5)
+      expect(spy).to have_received(:update_card_hours).twice
     end
   end
 end
