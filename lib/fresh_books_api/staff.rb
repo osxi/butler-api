916851e66dd532cefc189113e8132d187af27212
zach <zach@poeticsystems.com>
2014-11-21 11:17:27 -0600

    Add FreshBooksApi::Staff#import_all.

--- lib/fresh_books_api/staff.rb
+++ lib/fresh_books_api/staff.rb
@@ -3,5 +3,18 @@ module FreshBooksApi
     def all
       list_get_all(:staff, :staff_members, :member)
     end
+
+    def import_all
+      all_staff = all
+
+      all_staff.each do |staff|
+        employee = Employee.find_or_create_by(fb_staff_id: staff['staff_id'])
+        employee.first_name ||= staff['first_name']
+        employee.last_name  ||= staff['last_name']
+        employee.email      ||= staff['email']
+
+        employee.save! if employee.changed?
+      end
+    end
   end
 end
