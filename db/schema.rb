930b07c48541a5ea66ad3aa5f688fa4a554a13ec
zach <zach@poeticsystems.com>
2014-11-24 12:15:02 -0600

    Add :slack_id to User.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119193522) do
+ActiveRecord::Schema.define(version: 20141124180039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20141119193522) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "manager",         default: false
+    t.string   "slack_id"
   end
 
 end
