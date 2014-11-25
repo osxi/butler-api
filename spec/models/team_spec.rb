b5296582748b59c794a0ec9f27457b77c1c07fc7
zach <zach@poeticsystems.com>
2014-11-25 09:58:16 -0600

    Model tests for simple methods (why not?).

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Team, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:team) { FactoryGirl.create(:team) }
+  context 'has many' do
+    it 'users' do
+      expect(team).to have_many(:users)
+    end
+
+    it 'time_entries' do
+      expect(team).to have_many(:time_entries)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+
+  it '#name' do
+    full_name = "#{user.first_name} #{user.last_name}"
+    expect(user.name).to eq(full_name)
+  end
 end
