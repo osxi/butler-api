49a33e88c476e8b720c4aad2491d42640bc11260
zach <zach@poeticsystems.com>
2014-11-25 09:57:41 -0600

    SlackApi::Chat#post_message initial commit.

---
+++ spec/lib/slack_api/chat_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe SlackApi::Chat do
+  let (:slack) { SlackApi::Chat.new(ENV['SLACK_API_TOKEN']) }
+  
+  it 'can successfully send a message', vcr: true do
+    req = slack.post_message(channel: '#snitch', text: 'chat_spec.rb running.',
+                             username: 'RSpec')
+    expect(req.response.code).to eq('200')
+  end
+end
