69db9a5c9c03bcb28e221664c509c8b6242434cd
Jake Craige <jake@poeticsystems.com>
2014-12-15 11:39:34 -0600

    add get_actual and get_estimate to card parser

--- lib/trello/card_parser.rb
+++ lib/trello/card_parser.rb
@@ -1,6 +1,8 @@
 module Trello
   class CardParser < Struct.new(:name)
     SPLIT_HOURS_REGEX = /(.*\[)([\d\.]+)(\].*)\s?\z/
+    ACTUAL_REGEX      = /\[([\d\.]+)\]\s?\z/
+    ESTIMATE_REGEX    = /\A\(([\d\.]+)\)/
 
     def update_hours_in_name(total_hours)
       split_name = name.to_s.split(SPLIT_HOURS_REGEX)
@@ -11,6 +13,24 @@ module Trello
         split_name.join
       else
         "#{name} [#{total_hours.round(2)}]"
+      end
+    end
+
+    def get_actual
+      first_match_or(matches: name.match(ACTUAL_REGEX),
+                     default: 0.0).to_f
+    end
+
+    def get_estimate
+      first_match_or(matches: name.match(ESTIMATE_REGEX),
+                     default: 0.0).to_f
+    end
+
+    def first_match_or(matches:, default:)
+      if matches && matches.length
+        matches[1]
+      else
+        default
       end
     end
   end
--- spec/lib/trello/card_parser_spec.rb
+++ spec/lib/trello/card_parser_spec.rb
@@ -14,4 +14,50 @@ describe Trello::CardParser do
       it { is_expected.to eql 'Hello [1.0]' }
     end
   end
+
+  context '#get_estimate' do
+    context 'with hours' do
+      subject { Trello::CardParser.new('(0.5) Hello').get_estimate }
+
+      it { is_expected.to eql 0.5 }
+    end
+
+    context 'with hours and actual' do
+      subject { Trello::CardParser.new('(0.5) Hello [0.3]').get_estimate }
+
+      it { is_expected.to eql 0.5 }
+    end
+
+    context 'without hours' do
+      subject { Trello::CardParser.new('Hello').get_estimate }
+
+      it { is_expected.to eql 0.0 }
+    end
+  end
+
+  context '#get_actual' do
+    context 'with actual' do
+      subject { Trello::CardParser.new('Hello [0.35]').get_actual }
+
+      it { is_expected.to eql 0.35 }
+    end
+
+    context 'with actual and estimate' do
+      subject { Trello::CardParser.new('(0.5) Hello [0.35]').get_actual }
+
+      it { is_expected.to eql 0.35 }
+    end
+
+    context 'without hours' do
+      subject { Trello::CardParser.new('Hello').get_actual }
+
+      it { is_expected.to eql 0.0 }
+    end
+
+    context 'without hours / with estimate' do
+      subject { Trello::CardParser.new('(0.5) Hello').get_actual }
+
+      it { is_expected.to eql 0.0 }
+    end
+  end
 end
