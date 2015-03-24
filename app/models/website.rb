b34f6de422a6eb6bb197e33af78179f045c4fc1b
John Goodman <john@poeticsystems.com>
2015-03-23 23:32:03 -0500

    begin rescue

--- app/models/website.rb
+++ app/models/website.rb
@@ -5,12 +5,19 @@ class Website < ActiveRecord::Base
 
 	def ping_url
 		http = Net::HTTP.new(url,80)
-		response = http.request_get('/')
-		code = response.code
-		
-		live = code == '200' ? true : false
-		
-		self.update_column(:live, live)
-		self.update_column(:ping, Time.now)
+
+		begin
+			response = http.request_get('/')
+
+			code = response.code
+			
+			live = code == '200' ? true : false
+			
+			self.update_column(:live, live)
+			self.update_column(:ping, Time.now)
+
+		rescue
+			self.update_column(:live, false)
+		end
 	end
 end
